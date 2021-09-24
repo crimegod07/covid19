1adf0a8cbea27f68a8ce0319f96ed7492bdbefbc
crimegod <dpk.exe@gmail.com>
2021-09-24 21:20:29 +0530

    2nd change

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,7 @@
 import '@lwc/synthetic-shadow';
 import { createElement } from 'lwc';
 import MyApp from 'my/app';
-//hello
+//hello world
 const app = createElement('my-app', { is: MyApp });
 // eslint-disable-next-line @lwc/lwc/no-document-query
 document.querySelector('#main').appendChild(app);
